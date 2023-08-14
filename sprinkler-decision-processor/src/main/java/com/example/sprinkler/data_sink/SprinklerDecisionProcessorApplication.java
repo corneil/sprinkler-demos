d345311666541327a8702c1a445eebd90811bf64
Corneil du Plessis <pcorneil@vmware.com>
2023-08-14 17:48:25 +0200

    Remove explicit reference to FunctionProperties.

--- sprinkler-decision-processor/src/main/java/com/example/sprinkler/data_sink/SprinklerDecisionProcessorApplication.java
+++ sprinkler-decision-processor/src/main/java/com/example/sprinkler/data_sink/SprinklerDecisionProcessorApplication.java
@@ -7,13 +7,12 @@ import com.example.sprinkler.decision.TimerRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.function.context.FunctionProperties;
 import org.springframework.context.annotation.Import;
 import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
 @EnableIntegration
-@EnableConfigurationProperties({SprinklerDecisionProperties.class, FunctionProperties.class})
+@EnableConfigurationProperties({SprinklerDecisionProperties.class})
 @Import({SprinklerClientConfig.class, TimerRule.class})
 public class SprinklerDecisionProcessorApplication {
     public static void main(String[] args) {
--- sprinkler-timer-source/src/main/java/com/example/sprinkler/simulation_timer_source/SprinklerTimerSourceApplication.java
+++ sprinkler-timer-source/src/main/java/com/example/sprinkler/simulation_timer_source/SprinklerTimerSourceApplication.java
@@ -8,11 +8,10 @@ import com.example.sprinkler.simulation_timer.TimerSimulationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.function.context.FunctionProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({SimulationTimerConfiguration.class, SprinklerClientConfig.class, FunctionProperties.class})
+@Import({SimulationTimerConfiguration.class, SprinklerClientConfig.class})
 @EnableConfigurationProperties({SimulationClientProperties.class, TimerSimulationProperties.class})
 public class SprinklerTimerSourceApplication {
     public static void main(String[] args) {
