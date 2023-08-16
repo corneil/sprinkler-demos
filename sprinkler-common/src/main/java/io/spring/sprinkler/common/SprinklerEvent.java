ae606edfee093695846f422ffc6cc2dd6098c75c
Corneil du Plessis <pcorneil@vmware.com>
2023-08-17 00:18:11 +0200

    Improve handling of null events.

--- sprinkler-common/src/main/java/io/spring/sprinkler/common/SprinklerEvent.java
+++ sprinkler-common/src/main/java/io/spring/sprinkler/common/SprinklerEvent.java
@@ -2,8 +2,11 @@ package io.spring.sprinkler.common;
 
 import java.time.ZonedDateTime;
 
-import org.springframework.lang.Nullable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+import org.springframework.lang.Nullable;
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record SprinklerEvent(String id, ZonedDateTime timestamp, @Nullable SprinklerState state, @Nullable String reason) {
     public String getId() {
         return id;
