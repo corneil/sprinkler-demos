2eb8575fdb7f6b0233236ff69e5b5d8330d43a0f
Corneil du Plessis <pcorneil@vmware.com>
2023-08-18 11:43:25 +0200

    Update to publish images

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 MT="-T 0.5C"
 ./mvnw -B -s .settings.xml install verify $MT
-./mvnw -B -s .settings.xml -pl :sprinkler-simulation-app,:sprinkler-event-source,:sprinkler-decision-processor,:sprinkler-data-sink,:sprinkler-report spring-boot:build-image $MT
+./mvnw -B -s .settings.xml -pl :sprinkler-simulation-app,:sprinkler-event-source,:sprinkler-decision-processor,:sprinkler-data-sink,:sprinkler-report spring-boot:build-image -DskipTests $MT
 docker tag corneil/sprinkler-simulation-app:1.0.0-SNAPSHOT corneil/sprinkler-simulation-app:latest
 docker tag corneil/sprinkler-event-source:1.0.0-SNAPSHOT corneil/sprinkler-event-source:latest
 docker tag corneil/sprinkler-decision-processor:1.0.0-SNAPSHOT corneil/sprinkler-decision-processor:latest
