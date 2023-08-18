ea76f433c2ae66acd191e6f518d809f6f2860615
Corneil du Plessis <pcorneil@vmware.com>
2023-08-18 12:12:50 +0200

    Fix parent and repackage

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 MT="-T 0.5C"
 ./mvnw -B -s .settings.xml install verify $MT
 ./mvnw -B -s .settings.xml -pl :sprinkler-simulation-app,:sprinkler-event-source,:sprinkler-decision-processor,:sprinkler-data-sink,:sprinkler-report spring-boot:build-image -DskipTests $MT
-docker tag corneil/sprinkler-simulation-app:1.0.0-SNAPSHOT corneil/sprinkler-simulation-app:latest
-docker tag corneil/sprinkler-event-source:1.0.0-SNAPSHOT corneil/sprinkler-event-source:latest
-docker tag corneil/sprinkler-decision-processor:1.0.0-SNAPSHOT corneil/sprinkler-decision-processor:latest
-docker tag corneil/sprinkler-data-sink:1.0.0-SNAPSHOT corneil/sprinkler-data-sink:latest
-docker tag corneil/sprinkler-report:1.0.0 corneil/sprinkler-report:latest
+docker tag corneil/sprinkler-simulation-app:1.0.0-SNAPSHOT ghcr.io/corneil/sprinkler-demos/sprinkler-simulation-app:latest
+docker tag corneil/sprinkler-event-source:1.0.0-SNAPSHOT ghcr.io/corneil/sprinkler-demos/sprinkler-event-source:latest
+docker tag corneil/sprinkler-decision-processor:1.0.0-SNAPSHOT ghcr.io/corneil/sprinkler-demos/sprinkler-decision-processor:latest
+docker tag corneil/sprinkler-data-sink:1.0.0-SNAPSHOT ghcr.io/corneil/sprinkler-demos/sprinkler-data-sink:latest
+docker tag corneil/sprinkler-report:1.0.0 ghcr.io/corneil/sprinkler-demos/sprinkler-report:latest
--- push-images.sh
+++ push-images.sh
@@ -2,6 +2,6 @@
 IMAGES="sprinkler-simulation-app sprinkler-event-source sprinkler-decision-processor sprinkler-data-sink sprinkler-report"
 for IMG in $IMAGES; do
     echo "Pushing $IMG"
-    docker push "ghcr.io/corneil/$IMG:latest"
+    docker push "ghcr.io/corneil/sprinkler-demos/$IMG:latest"
     echo "Pushed $IMG"
 done
