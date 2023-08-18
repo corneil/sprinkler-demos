e83787a3c3761d47fb52245bf1c30d46a13682aa
Corneil du Plessis <pcorneil@vmware.com>
2023-08-18 11:15:31 +0200

    Update to publish images

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-docker push corneil/sprinkler-simulation-app:latest
-docker push corneil/sprinkler-event-source:latest
-docker push corneil/sprinkler-decision-processor:latest
-docker push corneil/sprinkler-data-sink:latest
-docker push corneil/sprinkler-report:latest
+IMAGES="sprinkler-simulation-app sprinkler-event-source sprinkler-decision-processor sprinkler-data-sink sprinkler-report"
+for IMG in $IMAGES; do
+    echo "Pushing $IMG"
+    docker push "ghcr.io/corneil/$IMG:latest"
+    echo "Pushed $IMG"
+done
